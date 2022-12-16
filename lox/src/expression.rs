1512b433e381a44e3944e4f584ed97c53710207b
Max Kießling <max@kopfueber.org>
2022-12-16 11:09:35 +0100

    Chapter 5: Add pretty printing method for expression trees

--- lox/src/expression.rs
+++ lox/src/expression.rs
@@ -1,3 +1,11 @@
+use std::fmt;
+use std::fmt::Formatter;
+
+use crate::expression::BinaryOp::*;
+use crate::expression::Expression::*;
+use crate::expression::LiteralType::*;
+use crate::expression::UnaryOp::*;
+
 enum Expression {
     Unary {
         inner: Box<Expression>,
@@ -16,11 +24,61 @@ enum Expression {
     },
 }
 
+impl Expression {
+    fn pretty(&self) -> String {
+        fn pretty(expr: &Expression, level: u32) -> String {
+            let mut prefix = if level == 0 {
+                "".to_string()
+            } else {
+                let mut prefix = "   ".repeat(level as usize);
+                prefix.push_str("|_ ");
+                prefix
+            };
+
+            let thing = match expr {
+                Unary { inner, op } => {
+                    format!("Unary {}\n{}", op, pretty(inner, level + 1))
+                }
+                Binary { left, right, op } => {
+                    format!(
+                        "Binary {}\n{}\n{}",
+                        op,
+                        pretty(left, level + 1),
+                        pretty(right, level + 1)
+                    )
+                }
+                Literal { value } => format!("{}", value),
+                Grouping { inner } => {
+                    format!("Group\n{}", pretty(inner, level + 1))
+                }
+                _ => "".to_string(),
+            };
+
+            prefix.push_str(&thing);
+
+            prefix
+        }
+
+        pretty(self, 0)
+    }
+}
+
 enum LiteralType {
     NumberLit { value: f64 },
     StringLit { value: String },
     TrueLit,
     FalseLit,
+}
+
+impl fmt::Display for LiteralType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            NumberLit { value } => write!(f, "{}", value),
+            StringLit { value } => write!(f, "\"{}\"", value),
+            TrueLit => write!(f, "true"),
+            FalseLit => write!(f, "false"),
+        }
+    }
 }
 
 enum BinaryOp {
@@ -36,7 +94,65 @@ enum BinaryOp {
     Divide,
 }
 
+impl fmt::Display for BinaryOp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Equals => write!(f, "=="),
+            NotEquals => write!(f, "!="),
+            LessThan => write!(f, "<"),
+            LessThanOrEquals => write!(f, "<="),
+            GreaterThan => write!(f, ">"),
+            GreaterThanOrEquals => write!(f, ">="),
+            Add => write!(f, "+"),
+            Subtract => write!(f, "-"),
+            Multiply => write!(f, "*"),
+            Divide => write!(f, "/"),
+        }
+    }
+}
+
 enum UnaryOp {
     Not,
     Negative,
+}
+
+impl fmt::Display for UnaryOp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Not => write!(f, "Not"),
+            Negative => write!(f, "Negative"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn foo() {
+        let expression = Binary {
+            left: Box::new(Binary {
+                left: Box::new(Literal {
+                    value: NumberLit { value: 42f64 },
+                }),
+                right: Box::new(Binary {
+                    left: Box::new(Literal {
+                        value: NumberLit { value: 42f64 },
+                    }),
+                    right: Box::new(Literal {
+                        value: NumberLit { value: 42f64 },
+                    }),
+                    op: Add,
+                }),
+                op: Add,
+            }),
+            right: Box::new(Literal {
+                value: NumberLit { value: 42f64 },
+            }),
+            op: Add,
+        };
+
+        println!("{}", expression.pretty());
+    }
 }
